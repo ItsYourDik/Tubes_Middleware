08156fd916628c392862aa6afefdaae4817b24f3
Fazril235 <arielff54@gmail.com>
2022-12-23 15:01:29 +0700

    Membuat halaman Home

---
+++ resources/views/dashboard/layouts/header.blade.php
@@ -0,0 +1,8 @@
+<div class="flex flex-col bg-white h-24 p-2 ">
+  <div class="flex flex-row space-x-3">
+
+    <h4 class="font-bold text-gray-500 p-1 text-4xl">Dashboard</h4>
+
+  </div>
+  <p class="text-gray-400 px-1 text-xl font-semibold">Hi Dik Dik Nur Illahi</p>
+</div>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  @vite('resources/js/script.js')
+  <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;0,800;1,400;1,900&display=swap" rel="stylesheet">
+  <title>VAKANTIE</title>
+</head>
+<body>
+  @include('partials.navbar2')
+  
+  <div class="container">
+    @yield('container')
+  </div>
+
+  @include('partials.footer')
+  
+</body>
+</html>
