7131104e25026a0991fc29c47575fcccf6da050c
SitiKomalasari14 <sitikomalasari1417@gmail.com>
2022-12-27 17:31:57 +0700

    mengerjakan halaman About

---
+++ resources/views/post.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.main')
+
+@section('container')
+        <div class="min-h-screen bg-cover bg-fixed" style="background-image: url(img/apa.jpg)"></div>
+        
+        <div class="h-screen mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <h1 class="font-Montserrat font-semibold text-xl">Apa sok</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illum excepturi maiores modi! Corrupti nobis nihil provident sit beatae obcaecati. Illo, quisquam quidem. Officia qui, illum, quae praesentium fuga porro ab amet quod tempore asperiores distinctio repellat aspernatur incidunt, quam quis ducimus corporis velit! Dolores qui ab laborum asperiores ducimus enim commodi exercitationem corporis assumenda quae odio maxime, ipsum illum animi fugiat illo vero tempore, error facere suscipit ut! Quam amet voluptatum, et, molestias quo itaque a, aliquid aut iste deserunt beatae nostrum? Eius vel consequatur rem ea consectetur accusantium in ratione quidem nihil consequuntur, eaque repudiandae! Quia, facere sapiente!</p>
+        </div>
+        
+        <div class="min-h-screen bg-cover bg-fixed" style="background-image: url(img/jumbotron.jpg)"></div>
+
+        <div class="h-screen mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <h1 class="font-Montserrat font-semibold text-xl">Apa sok</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illum excepturi maiores modi! Corrupti nobis nihil provident sit beatae obcaecati. Illo, quisquam quidem. Officia qui, illum, quae praesentium fuga porro ab amet quod tempore asperiores distinctio repellat aspernatur incidunt, quam quis ducimus corporis velit! Dolores qui ab laborum asperiores ducimus enim commodi exercitationem corporis assumenda quae odio maxime, ipsum illum animi fugiat illo vero tempore, error facere suscipit ut! Quam amet voluptatum, et, molestias quo itaque a, aliquid aut iste deserunt beatae nostrum? Eius vel consequatur rem ea consectetur accusantium in ratione quidem nihil consequuntur, eaque repudiandae! Quia, facere sapiente!</p>
+        </div>
+
+        <div class="min-h-screen bg-cover bg-fixed" style="background-image: url(img/rafting.jpg)"></div>
+
+        <div class="h-screen mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <h1 class="font-Montserrat font-semibold text-xl">Apa sok</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illum excepturi maiores modi! Corrupti nobis nihil provident sit beatae obcaecati. Illo, quisquam quidem. Officia qui, illum, quae praesentium fuga porro ab amet quod tempore asperiores distinctio repellat aspernatur incidunt, quam quis ducimus corporis velit! Dolores qui ab laborum asperiores ducimus enim commodi exercitationem corporis assumenda quae odio maxime, ipsum illum animi fugiat illo vero tempore, error facere suscipit ut! Quam amet voluptatum, et, molestias quo itaque a, aliquid aut iste deserunt beatae nostrum? Eius vel consequatur rem ea consectetur accusantium in ratione quidem nihil consequuntur, eaque repudiandae! Quia, facere sapiente!</p>
+        </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PostColltroller;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +23,67 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        "title" => "Home",
+        'active' => 'home'
+    ]);
 });
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        'active' => 'about',
+        "name" => "Dik Dik Nur Illahi",
+        "email" => "Dikdiknurillahi17@gmail.com",
+        "image" => "dik.jpg"
+    ]);
+});
+
+
+
+Route::get('/posts', [PostColltroller::class, 'index']);
+//halaman single posts
+Route::get('/posts/{post:slug}', [PostColltroller::class, 'show']);
+
+
+Route::get('/categories', function () {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'active' => 'categories',
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
+
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
+
+
+
+// Route::get('/categories/{category:slug}', function (Category $category) {
+//     return view('posts', [
+//         'title' => "Post By Category : $category->name",
+//         'active' => 'categories',
+//         'posts' => $category->posts->load('author', 'category')
+//     ]);
+// });
+
+// Route::get('/authors/{author:username}', function (User $author) {
+//     return view('posts', [
+//         'title' => "Post By Author : $author->name",
+//         'active' => 'author',
+//         'posts' => $author->posts->load('category', 'author')
+//     ]);
+// });
